96c744bbc0bc3a8be52b8d0756b1534287610f71
Wahidyan Kresna Fridayoka <wahidyankf@gmail.com>
2023-03-24 17:39:19 +0700

    add ch05

--- a-javascript-action/.github/actions/hello-world/index.js
+++ a-javascript-action/.github/actions/hello-world/index.js
@@ -7,17 +7,17 @@ const thirdGreeting = core.getInput("third-greeting");
 const lastOneGreeted = core.getInput("last-one-greeted");
 
 async function run() {
-  try {
-    if (firstGreeting) {
-      core.setOutput("last-one-greeted", firstGreeting);
-    } else if (secondGreeting) {
-      core.setOutput("last-one-greeted", secondGreeting);
-    } else if (thirdGreeting) {
-      core.setOutput("last-one-greeted", thirdGreeting);
+    try {
+        if (firstGreeting) {
+            core.setOutput("last-one-greeted", firstGreeting); 
+        } else if (secondGreeting) {
+            core.setOutput("last-one-greeted", secondGreeting);
+        } else if (thirdGreeting) {
+            core.setOutput("last-one-greeted", thirdGreeting);
+        }
+    } catch (error) {
+      core.setFailed(error.message);
     }
-  } catch (error) {
-    core.setFailed(error.message);
-  }
 }
 
 run();
